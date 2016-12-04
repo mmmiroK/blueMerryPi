137b277415378ab3d9f974d1a2511799ba408d0c
needAdoctor <xryhwdst@163.com>
2016-12-04 12:02:35 +0800

    fst

---
+++ mobile/script/controllers/personSet.js
@@ -0,0 +1,20 @@
+/**
+ * Created by zbacao on 2016/8/16.
+ */
+lmapp.controller("personSet",["$scope",function($scope){
+        $scope.return=function(){
+            window.history.back()
+        };
+        $scope.num=false;
+        $scope.sexSet=function(){
+            $scope.num=true;
+            console.log($scope.num);
+         }
+        $scope.down=function(){
+            $scope.num=false;
+            console.log($scope.num);
+        }
+
+
+
+}]);
---
+++ script/common.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Administrator on 2016-08-19.
+ */
+//” œ‰≈–∂œ
+function testEmail(email) {
+    var a = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-z][a-z.]{2,8}$/;
+    return a.test(email)
+}
+
+// ÷ª˙∫≈¬Î≈–∂œ
+function chkPhone(phoneNum){
+    var re = /^(13[0-9]|14[0-9]|15[0-9]|17[0-9]|18[0-9])\d{8}$/i;
+    var phone = phoneNum;
+    if(!re.test(phone)){
+        return false;
+    }else if(phone.length < 11){
+        return false;
+    }else if(phone.length>11){
+        return false;
+    }
+    return true;
+}
+
+//”√ªß√˚≈–∂œ
+function checkUsername(name){
+    var n=/^[a-zA-Z]\w{6,12}$/ig;
+    return n.test(name);
+}
+//√‹¬Î≈–∂œ
+function pwdRight(pwdBox,pwdFromDB){
+    if(pwdBox.val()===pwdFromDB){
+        return true;
+    }else{
+        return false;
+    }
+}
+
